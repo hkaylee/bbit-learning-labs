e42ac4e0469496a58e5842880688c84f1a85727e
Kaylee Hou <kayleeh@usc.edu>
2025-03-07 03:28:50 +0000

    part 3

--- Tech-Lab-On-Campus/NewsFeed/frontend/src/components/NewsFeed.tsx
+++ Tech-Lab-On-Campus/NewsFeed/frontend/src/components/NewsFeed.tsx
@@ -7,24 +7,16 @@ interface NewsFeedProps {
 }
 
 // The NewsFeed component receives an array of Article objects and renders them in a grid.
-function NewsFeed({ articles }: NewsFeedProps ) {
-    // PART 3: Populate a news feed with the given `articles`
-
-    // Now that you've implemented a reusable NewsCard in Part 2, you can use that to build out
-    // the news feed underneath the FeaturedNews section.
-
-    // Hint: Array.map() may be useful here: https://www.geeksforgeeks.org/typescript-array-map-method/
-
+function NewsFeed({ articles }: NewsFeedProps) {
     return (
         <div className="stories-container">
-            <div className="stories-grid grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+            <div className="stories-grid grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6">
                 {articles.map((article, index) => (
                     <NewsCard key={index} article={article} />
                 ))}
             </div>
         </div>
     );
-};
+}
 
-
-
+export default NewsFeed;
